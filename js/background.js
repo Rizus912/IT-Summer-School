6e89cba0cc75fa9a996308533b4ea373ec6f4320
Koshechkin Vadim <koshechkin.1988@mail.ru>
2017-07-14 15:49:18 +0300

    add first version of site

---
+++ js/background.js
@@ -0,0 +1,12 @@
+
+
+$(function() {
+    $('#home').vegas({
+        delay:5000,
+		slides: [
+            { src: 'IMG/1.png', fade:1000 },
+            { src: 'IMG/2.png', fade:1000 },
+            { src: 'IMG/3.png', fade:1000 }
+        ]
+    });
+});
